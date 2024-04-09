7570194a14619481fb379c5abdd403aca6d26119
akshayyy22 <akshaypersonalstudentid@gmail.com>
2024-04-09 22:05:05 +0530

    4th Part Completed - Socket

--- frontend/src/components/sidebar/SearchInput.jsx
+++ frontend/src/components/sidebar/SearchInput.jsx
@@ -29,25 +29,25 @@ const SearchInput = () => {
   return (
     <form onSubmit={handleSubmit} className="flex items-center gap-2">
       <label
-        class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
-        for="default-search"
+        className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+        htmlFor="default-search"
       >
         Search
       </label>
-      <div class="relative">
-        <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+      <div className="relative">
+        <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
           <svg
             viewBox="0 0 20 20"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
             aria-hidden="true"
-            class="w-4 h-4 text-gray-500 dark:text-gray-400"
+            className="w-4 h-4 text-gray-500 dark:text-gray-400"
           >
             <path
               d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-              stroke-width="2"
-              stroke-linejoin="round"
-              stroke-linecap="round"
+              strokeWidth="2"
+              strokeLinejoin="round"
+              strokeLinecap="round"
               stroke="currentColor"
             ></path>
           </svg>
@@ -55,47 +55,32 @@ const SearchInput = () => {
         <input
           required=""
           placeholder="Search"
-          class="block w-full p-4 py-5 ps-10 text-lg text-slate-900 border border-slate-300 rounded-lg bg-slate-50 focus:ring-slate-600 outline-none focus:border-slate-500 dark:bg-slate-700 dark:border-slate-600 dark:placeholder-slate-400 dark:text-slate-300 dark:focus:ring-slate-500 dark:focus:border-slate-500"
+          className="block w-full p-4 py-5 ps-10 text-lg text-slate-900 border border-slate-300 rounded-lg bg-slate-50 focus:ring-slate-600 outline-none focus:border-slate-500 dark:bg-slate-700 dark:border-slate-600 dark:placeholder-slate-400 dark:text-slate-300 dark:focus:ring-slate-500 dark:focus:border-slate-500"
           id="default-search"
           type="search"
           value={search}
           onChange={(e) => setSearch(e.target.value)}
         />
-        <button class="absolute end-2.5 bottom-1/2 translate-y-1/2 p-4 text-sm font-medium text-white bg-slate-500 rounded-lg border border-slate-300 hover:bg-slate-400 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+        <button className="absolute end-2.5 bottom-1/2 translate-y-1/2 p-4 text-sm font-medium text-white bg-slate-500 rounded-lg border border-slate-300 hover:bg-slate-400 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
           <svg
             viewBox="0 0 20 20"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
             aria-hidden="true"
-            class="w-4 h-4"
+            className="w-4 h-4"
           >
             <path
               d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-              stroke-width="2"
-              stroke-linejoin="round"
-              stroke-linecap="round"
+              strokeWidth="2"
+              strokeLinejoin="round"
+              strokeLinecap="round"
               stroke="currentColor"
             ></path>
           </svg>
-          <span class="sr-only">Search</span>
+          <span className="sr-only">Search</span>
         </button>
       </div>
     </form>
   );
 };
 export default SearchInput;
-
-// STARTER CODE SNIPPET
-// import { IoSearchSharp } from "react-icons/io5";
-
-// const SearchInput = () => {
-// 	return (
-// 		<form className='flex items-center gap-2'>
-// 			<input type='text' placeholder='Search…' className='input input-bordered rounded-full' />
-// 			<button type='submit' className='btn btn-circle bg-sky-500 text-white'>
-// 				<IoSearchSharp className='w-6 h-6 outline-none' />
-// 			</button>
-// 		</form>
-// 	);
-// };
-// export default SearchInput;
